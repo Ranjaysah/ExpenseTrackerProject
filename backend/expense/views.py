501a4ae3afa8a558446fdd992330ac5546e1c881
Ranjaysah <Ranjaywinsahil@yahoo.com>
2026-03-08 22:03:11 +0530

    add urls file and signup function in view

--- backend/expense/views.py
+++ backend/expense/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt 
+import json
+from .models import *
 
 # Create your views here.
+
+#Singup API
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        fullname = data.get('Fullname')
+        email = data.get('Email')
+        password = data.get('Password')
+
+        if UserDetail.objects.filter(Email = email).exists():
+            return JsonResponse({'message':'Email already exists'}, status=400)
+        UserDetail.objects.create(Fullname=fullname, Email=email, Password=password)
+        return JsonResponse({'message':'User registered Successfully'}, status=201)
+    
+
+
+ 
+
+
+
+
