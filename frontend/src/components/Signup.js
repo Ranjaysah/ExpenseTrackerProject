d340e8e3f5afc660744b61e0573eb924bca0ba62
Ranjaysah <Ranjaywinsahil@yahoo.com>
2026-04-30 23:09:16 +0530

    Create singup page and changes

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Signup = () => {
   return (
@@ -11,8 +13,11 @@ const Signup = () => {
           Create your account to start tracking expenses
         </p>
       </div>
-      <form>
-        <div className="">
+      <form
+        className="p-4 rounded shadow mx-auto"
+        style={{ maxWidth: "400px" }}
+      >
+        <div className="mb-3">
           <label className="form-label">FullName</label>
           <div className="input-group">
             <span className="input-group-text">
@@ -27,7 +32,43 @@ const Signup = () => {
             />
           </div>
         </div>
+        <div className="mb-3">
+          <label className="form-label">Email</label>
+          <div className="input-group">
+            <span className="input-group-text">
+              <i className="fas fa-envelope"></i>
+            </span>
+            <input
+              type="text"
+              name="Email"
+              className="form-control"
+              required
+              placeholder="Enter your Email"
+            />
+          </div>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <div className="input-group">
+            <span className="input-group-text">
+              <i className="fas fa-lock"></i>
+            </span>
+            <input
+              type="password"
+              name="Password"
+              className="form-control"
+              required
+              placeholder="Create your password"
+            />
+          </div>
+        </div>
+        <button type="submit" className="btn btn-primary w-100 mt-3">
+          <i className="fas fa-user-plus me-2"></i>
+          Signup
+        </button>
       </form>
+
+      <ToastContainer />
     </div>
   );
 };
